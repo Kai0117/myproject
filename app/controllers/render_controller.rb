3d6f16f1a054798e2c481317b36dd7fc776d0b99
= <=>
2024-09-07 11:18:50 +0900

    change views

--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -1,4 +1,6 @@
 class RenderController < ApplicationController
   def index
   end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "index" => "render#index"
+  get "/" => "render#index"
+  get "about" => "render#about"
   
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
