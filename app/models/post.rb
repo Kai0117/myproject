1acc6415d98e03846e8ba4e0890f6b5cd29f938e
= <=>
2024-10-11 09:56:08 +0900

    change stylesheet

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
-end
+    validates :content, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z\s]+\z/ }
+  end
+  
+
+
+#class Post < ApplicationRecord
+ #   validates :content, {presence: true, length: {maximum: 10}},
+  #  validates :content, format: { with: /\A[a-zA-Z]+\z/, message: "はアルファベットのみ入力可能です" }
+#end
